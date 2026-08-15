46bc713907d815e91e0d0373d8ec808b1b0e7368
eitikobata <eiti.kun@gmail.com>
2026-08-15 02:21:36 -0300

    redis fix

--- backend/src/common/redis/redis.module.ts
+++ backend/src/common/redis/redis.module.ts
@@ -15,6 +15,7 @@ export const REDIS_CLIENT = 'REDIS_CLIENT';
           host: config.get<string>('REDIS_HOST', 'localhost'),
           port: config.get<number>('REDIS_PORT', 6379),
           db: config.get<number>('REDIS_DB', 0),
+          password: config.get<string>('REDIS_PASSWORD') || undefined,
           maxRetriesPerRequest: null,
         });
       },
