c1e0985d7eafa80d0a2462703aa3dc9f4c825d70
eitikobata <eiti.kun@gmail.com>
2026-08-19 01:44:40 -0300

    fix

--- backend/src/k-stream/k-stream.module.ts
+++ backend/src/k-stream/k-stream.module.ts
@@ -3,9 +3,10 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { SimulatorService } from './simulator.service';
 import { KStreamService } from './k-stream.service';
 import { KStreamController } from './k-stream.controller';
+import { KIdModule } from '../k-id/k-id.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(), KIdModule],
   controllers: [KStreamController],
   providers: [SimulatorService, KStreamService],
   exports: [KStreamService],
